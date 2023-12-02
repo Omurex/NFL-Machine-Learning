a38d0f3469b8a7e336bb18a7c8bb82ea34537a8c
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2023-12-02 14:16:51 -0500

    Figured out how linear regression using sklearn

--- linearRegression.py
+++ linearRegression.py
@@ -1,5 +1,21 @@
 import csv
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
+
+def linearRegression():
+
+    X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+    y = np.dot(X, np.array([1, 2])) + 3
+    
+    linReg = LinearRegression().fit(X, y)
+    print(y)
+    print(linReg.predict([[1, 3], [2, 8]]))
+
+    print(linReg.score([[1,3], [2,9]], [10, 21]))
+
+
+linearRegression()
 
 # TODO: fix this to read in correct data
 def readCSV(file_path: Path) -> tuple[list[float], list[float]]:
