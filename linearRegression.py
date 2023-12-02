6fb83d79c4536353ba198389280c370c597e94da
anandas-b <79864434+anandas-b@users.noreply.github.com>
2023-12-02 14:42:02 -0500

    three different data sets
    no_na_data: removed every single data point that had an "NA" in the column
    na_to_0_data: replaced every instance of "NA" with a 0
    na_to_avg_data: replaced every instance of "NA" with the column average

--- linearRegression.py
+++ linearRegression.py
@@ -1,14 +1,58 @@
 import csv
+from pathlib import Path
+from typing import List
 
 
-# TODO: fix this to read in correct data
-def readCSV(file_path: Path) -> tuple[list[float], list[float]]:
+# Read all the data from the CSV file and put into an array
+def readCSV(file_path: Path) -> list[list]:
     with open(file_path, 'r') as f:
         reader = csv.reader(f)
         header = next(reader)
-        xs = []
-        ys = []
+        selected_cols = [4, 5, 6, 7, 8, 9, 10, 11]
+        data = []
+
         for row in reader:
-            xs.append(float(row[0]))
-            ys.append(float(row[1]))
-        return (xs, ys)
+            row_vals = [row[col] for col in selected_cols]
+            data.append(row_vals)
+        return data
+
+
+# Delete any data points that have "NA" for any of the physical tests
+# Completed with the help of ChatGPT
+def remove_na_players(data) -> list[list[float]]:
+    filtered_data = [[float(val) for val in row] for row in data if "NA" not in row]
+    return filtered_data
+
+
+# Replace every instance of "NA" with a 0
+# Completed with the help of ChatGPT
+def replace_na_with_zero(data) -> list[list[float]]:
+    filtered_data = [[float(0) if val == "NA" else float(val) for val in row] for row in data]
+    return filtered_data
+
+
+# Replace every instance of "NA" with the column average
+# Completed with the help of ChatGPT
+def replace_na_with_averages(no_na_data, data) -> list[list[float]]:
+    col_avg = [sum(col) / len(col) for col in no_na_data]
+    filtered_data = [[col_avg if val == "NA" else float(val) for val, col_avg in zip(row, col_avg)] for row in data]
+    return filtered_data
+
+
+if __name__ == '__main__':
+    p = Path(__file__).with_name('NFL.csv')
+
+    # Contains all data points, don't use
+    full_data = readCSV(p.absolute())
+
+    # Contains only the data points with every column completed
+    no_na_data = remove_na_players(full_data)
+    print(no_na_data[:5])
+
+    # Changed all "NA" data points to be 0
+    na_to_0_data = replace_na_with_zero(full_data)
+    print(na_to_0_data[:5])
+
+    # Changed all "NA" data points to be the column average
+    na_to_avg_data = replace_na_with_averages(no_na_data, full_data)
+    print(na_to_avg_data[:5])
