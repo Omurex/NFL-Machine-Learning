3e05eb02600fba7dee0ca5c9b2d072cdcab811c2
Hunter Gale <hunter137012@gmail.com>
2023-12-04 12:57:43 -0500

    Added Random Forest Regression

--- linearRegression.py
+++ linearRegression.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from typing import List
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 import re
 import random
 
@@ -17,15 +18,15 @@ def readCSV(file_path: Path) -> tuple[list[list[float]], list[int]]:
         # for index, header in enumerate(headers):
         #     print(f"{header}: {index}")
         selected_cols = [4, 5, 6, 7, 8, 9, 10, 11, 13]
-        pick_data_col = 12 # Column that pick data can be found in
+        pick_data_col = 12  # Column that pick data can be found in
         data = []
 
-        player_data = [] # Holds return data for player stats
-        pick_number = [] # Holds return for pick number
+        player_data = []  # Holds return data for player stats
+        pick_number = []  # Holds return for pick number
 
         for row in reader:
             pick_str = row[pick_data_col]
-            
+
             if pick_str == "NA":
                 continue
 
@@ -34,10 +35,9 @@ def readCSV(file_path: Path) -> tuple[list[list[float]], list[int]]:
             # Example: Arizona Cardinals / 1st / 31st pick / 2009
             pick_str = pick_str.split("/")[2]
             pick_number.append(int(re.sub('\D', '', pick_str)))
-                        
+
             row_vals = [row[col] for col in selected_cols]
             player_data.append(row_vals)
-
 
         return player_data, pick_number
 
@@ -71,8 +71,7 @@ def replace_na_with_averages(no_na_data, data) -> list[list[float]]:
     return filtered_data
 
 
-def calculateLinearRegression(playerTrainingData : list[list[float]], playerDraftPick : list[int]) -> LinearRegression:
-
+def calculateLinearRegression(playerTrainingData: list[list[float]], playerDraftPick: list[int]) -> LinearRegression:
     # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
 
     # X = Array of arrays of data (will be height, weight, long jump dist, etc. in each inner array)
@@ -83,33 +82,49 @@ def calculateLinearRegression(playerTrainingData : list[list[float]], playerDraf
 
     # X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
     # y = np.dot(X, np.array([1, 2])) + 3
-    
+
     linReg = LinearRegression().fit(playerTrainingData, playerDraftPick)
 
-    #print(playerDraftPick)
-    #print(linReg.predict([[1, 3], [2, 8]]))
+    # print(playerDraftPick)
+    # print(linReg.predict([[1, 3], [2, 8]]))
 
-    #print(linReg.score([[1,3], [2,9]], [10, 21]))
+    # print(linReg.score([[1,3], [2,9]], [10, 21]))
 
     return linReg
 
 
+def calculateRandomForestRegression(playerTrainingData: list[list[float]],
+                                    playerDraftPick: list[int]) -> RandomForestRegressor:
+    """
+    Train a random forest regression model to predict draft pick numbers based on player training data.
+    """
+
+    # Scikit Random Forest Regressor Documentation:
+    # https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestRegressor.html
+    rfRegressor = RandomForestRegressor(n_estimators=100, random_state=8)
+
+    # Fit the regressor to the training data
+    rfRegressor.fit(playerTrainingData, playerDraftPick)
+
+    return rfRegressor
+
+
 # The problem with this function is that we are trying to make a straight line in two dimensions out of a straight line in 8 dimensions. So, not very useful as a visual
 # xAxisDataIndex : Index in array of parsed data that should be used as the x axis variable when displaying linear regression line
-def plotLinearRegression(linearRegression : LinearRegression, playerTrainingData : list[list[float]], playerDraftPick : list[int], xAxisDataIndex : int):
-
-    scatterXData : list[float] = []
+def plotLinearRegression(linearRegression: LinearRegression, playerTrainingData: list[list[float]],
+                         playerDraftPick: list[int], xAxisDataIndex: int):
+    scatterXData: list[float] = []
 
     for data in playerTrainingData:
         scatterXData.append(data[xAxisDataIndex])
 
-    lineYData : list[float] = []
+    lineYData: list[float] = []
 
     print(linearRegression.coef_)
     print(linearRegression.intercept_)
 
-    #for i in scatterXData:
-        #lineYData.append(linearRegression.coef_[xAxisDataIndex] * i + linearRegression.intercept_)
+    # for i in scatterXData:
+    # lineYData.append(linearRegression.coef_[xAxisDataIndex] * i + linearRegression.intercept_)
 
     # for i in range(len(scatterXData)):
     #     lineYData.append(linearRegression.predict([playerTrainingData[i]]))
@@ -124,14 +139,13 @@ def plotLinearRegression(linearRegression : LinearRegression, playerTrainingData
     plt.show()
 
     pass
-    
+
 
 # testX = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
 # testY = np.dot(testX, np.array([1, 2])) + 3
 
 # linReg = calculateLinearRegression(testX, testY)
 # plotLinearRegression(linReg, testX, testY, 0)
-
 
 
 if __name__ == '__main__':
@@ -146,16 +160,15 @@ if __name__ == '__main__':
 
     # Contains only the data points with every column completed
     no_na_player_data, no_na_pick_data = remove_na_players(full_player_data, pick_data)
-    #print(no_na_player_data[:5])
+    # print(no_na_player_data[:5])
 
     # Changed all "NA" data points to be 0
     na_to_0_data = replace_na_with_zero(full_player_data)
-    #print(na_to_0_data[:5])
+    # print(na_to_0_data[:5])
 
     # Changed all "NA" data points to be the column average
     na_to_avg_data = replace_na_with_averages(no_na_player_data, full_player_data)
-    #print(na_to_avg_data[:5])
-
+    # print(na_to_avg_data[:5])
 
     train_data = no_na_player_data[:800]
     train_answers = no_na_pick_data[:800]
@@ -181,8 +194,8 @@ if __name__ == '__main__':
     print(linReg.score(test_data, test_answers))
     print(str(linReg.predict([test_data[0]])) + " : " + str(test_answers[0]))
 
-    #linReg = calculateLinearRegression(no_na_player_data, no_na_pick_data)
-    #plotLinearRegression(linReg, no_na_player_data, no_na_pick_data, 1)
+    # linReg = calculateLinearRegression(no_na_player_data, no_na_pick_data)
+    # plotLinearRegression(linReg, no_na_player_data, no_na_pick_data, 1)
 
     # testX = np.array([[1, 1], [1.5, 2], [2, 6], [5, 3]])
     # testY = np.dot(testX, np.array([1])) + 3
@@ -190,3 +203,16 @@ if __name__ == '__main__':
 
     # testLinReg = calculateLinearRegression(testX, testY)
     # plotLinearRegression(testLinReg, testX, testY, 0)
+
+    # Running random forest on N/A replaced by Col AVG
+    forest_test_data = na_to_avg_data[1000:]
+    forest_test_answers = pick_data[1000:]
+
+    rfReg = calculateRandomForestRegression(train_data, train_answers)
+    predictions = rfReg.predict(forest_test_data)
+    score = rfReg.score(forest_test_data, forest_test_answers)
+
+    # Output results to console
+    print(f'------------------------')
+    print(f'Random Forest Results:')
+    print(f'Random Forest R^2: {score}')
