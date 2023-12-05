94d916286c38c525440cf2b8c8717b8cc33ce391
anandas-b <79864434+anandas-b@users.noreply.github.com>
2023-12-04 19:19:58 -0500

    messing around with the code

--- linearRegression.py
+++ linearRegression.py
@@ -1,11 +1,11 @@
+import re
 import csv
+import random
 import numpy as np
 import matplotlib.pyplot as plt
-from pathlib import Path
 from typing import List
+from pathlib import Path
 from sklearn.linear_model import LinearRegression
-import re
-import random
 
 
 # Read all the data from the CSV file and put into an array
@@ -17,11 +17,11 @@ def readCSV(file_path: Path) -> tuple[list[list[float]], list[int]]:
         # for index, header in enumerate(headers):
         #     print(f"{header}: {index}")
         selected_cols = [4, 5, 6, 7, 8, 9, 10, 11, 13]
-        pick_data_col = 12 # Column that pick data can be found in
+        pick_data_col = 12  # Column that pick data can be found in
         data = []
 
-        player_data = [] # Holds return data for player stats
-        pick_number = [] # Holds return for pick number
+        player_data = []    # Holds return data for player stats
+        pick_number = []    # Holds return for pick number
 
         for row in reader:
             pick_str = row[pick_data_col]
@@ -56,11 +56,11 @@ def remove_na_players(player_data, pick_data) -> list[list[float]]:
     return filtered_player_data, filtered_pick_data
 
 
-# Replace every instance of "NA" with a 0
-# Completed with the help of ChatGPT
-def replace_na_with_zero(data) -> list[list[float]]:
-    filtered_data = [[float(0) if val == "NA" else float(val) for val in row] for row in data]
-    return filtered_data
+# # Replace every instance of "NA" with a 0
+# # Completed with the help of ChatGPT
+# def replace_na_with_zero(data) -> list[list[float]]:
+#     filtered_data = [[float(0) if val == "NA" else float(val) for val in row] for row in data]
+#     return filtered_data
 
 
 # Replace every instance of "NA" with the column average
@@ -71,27 +71,9 @@ def replace_na_with_averages(no_na_data, data) -> list[list[float]]:
     return filtered_data
 
 
-def calculateLinearRegression(playerTrainingData : list[list[float]], playerDraftPick : list[int]) -> LinearRegression:
-
-    # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
-
-    # X = Array of arrays of data (will be height, weight, long jump dist, etc. in each inner array)
-    # y = Array of predicted values (will be pick #)
-    # LinearRegression().fit() = Calculating the actual linear regression line
-    # .predict() = Uses linear regression line to predict y values using passed in X
-    # .score() = Calculates R^2 given X and real y
-
-    # X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-    # y = np.dot(X, np.array([1, 2])) + 3
-    
-    linReg = LinearRegression().fit(playerTrainingData, playerDraftPick)
-
-    #print(playerDraftPick)
-    #print(linReg.predict([[1, 3], [2, 8]]))
-
-    #print(linReg.score([[1,3], [2,9]], [10, 21]))
-
-    return linReg
+def calculate_linear_regression(player_training_data: list[list[float]], player_draft_pick: list[int]) -> LinearRegression:
+    lin_reg = LinearRegression().fit(player_training_data, player_draft_pick)
+    return lin_reg
 
 
 # The problem with this function is that we are trying to make a straight line in two dimensions out of a straight line in 8 dimensions. So, not very useful as a visual
@@ -133,7 +115,6 @@ def plotLinearRegression(linearRegression : LinearRegression, playerTrainingData
 # plotLinearRegression(linReg, testX, testY, 0)
 
 
-
 if __name__ == '__main__':
     p = Path(__file__).with_name('NFL.csv')
 
@@ -149,7 +130,7 @@ if __name__ == '__main__':
     #print(no_na_player_data[:5])
 
     # Changed all "NA" data points to be 0
-    na_to_0_data = replace_na_with_zero(full_player_data)
+    #na_to_0_data = replace_na_with_zero(full_player_data)
     #print(na_to_0_data[:5])
 
     # Changed all "NA" data points to be the column average
@@ -163,21 +144,21 @@ if __name__ == '__main__':
     test_data = no_na_player_data[800:]
     test_answers = no_na_pick_data[800:]
 
-    # # train_data = na_to_0_data[:800]
-    # # train_answers = pick_data[:800]
+    # train_data = na_to_0_data[:800]
+    # train_answers = pick_data[:800]
 
-    # # test_data = na_to_0_data[800:]
-    # # test_answers = pick_data[800:]
+    # test_data = na_to_0_data[800:]
+    # test_answers = pick_data[800:]
 
-    # # train_data = na_to_avg_data[:1000]
-    # # train_answers = pick_data[:1000]
+    # train_data = na_to_avg_data[:1000]
+    # train_answers = pick_data[:1000]
 
-    # # test_data = na_to_avg_data[1000:]
-    # # test_answers = pick_data[1000:]
+    # test_data = na_to_avg_data[1000:]
+    # test_answers = pick_data[1000:]
 
     # print(len(no_na_player_data))
 
-    linReg = calculateLinearRegression(train_data, train_answers)
+    linReg = calculate_linear_regression(train_data, train_answers)
     print(linReg.score(test_data, test_answers))
     print(str(linReg.predict([test_data[0]])) + " : " + str(test_answers[0]))
 
